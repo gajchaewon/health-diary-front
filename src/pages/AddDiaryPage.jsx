a9bef4a8cbd8960ecd95c52d7082490ed4736d5e
CHAEEUNPAKR <pce2416@gmail.com>
2024-02-01 15:54:05 +0900

    #6 - 사진 업로드 UI
    사진 업로드, 미리보기 기능 UI 디자인

--- src/pages/AddDiaryPage.jsx
+++ src/pages/AddDiaryPage.jsx
@@ -7,16 +7,40 @@ import SmartDisplayRoundedIcon from "@mui/icons-material/SmartDisplayRounded";
 import LockRoundedIcon from "@mui/icons-material/LockRounded";
 import LockOpenRoundedIcon from "@mui/icons-material/LockOpenRounded";
 import PhotoRoundedIcon from "@mui/icons-material/PhotoRounded";
+import DeleteIcon from "@mui/icons-material/Delete";
 import TagInput from "../components/TagInput";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 
 const AddDiaryPage = () => {
   const [isPublic, setIsOpen] = useState(false);
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
   const onLockClicked = () => {
-    if (isPublic === true) {
-      setIsOpen(false);
-    } else {
-      setIsOpen(true);
-    }
+    setIsOpen(!isPublic);
+  };
+
+  const handleImageUpload = (event) => {
+    const files = Array.from(event.target.files);
+    const urlList = files.map((file) => URL.createObjectURL(file));
+    setImages([...images, ...urlList]);
+  };
+
+  const handleImageDelete = () => {
+    //사진 삭제 api 넣기(실제 삭제 기능은 구현되지x)
+    const updatedImages = [...images];
+    updatedImages.splice(currentPage, 1);
+    setImages(updatedImages);
+    setCurrentPage((prevPage) => (prevPage > 0 ? prevPage - 1 : 0));
+  };
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
   };
 
   return (
@@ -37,12 +61,6 @@ const AddDiaryPage = () => {
       </S.ContentWrapper>
       <S.TagnIconsContainer>
         <S.TagWrapper>
-          {/* <TextField
-            id="outlined-multiline-flexible"
-            label="태그"
-            color="grey"
-            fullWidth
-          /> */}
           태그
           <TagInput />
         </S.TagWrapper>
@@ -61,9 +79,19 @@ const AddDiaryPage = () => {
           <MoreTimeRoundedIcon
             sx={{ fontSize: 40, color: "##212121", cursor: "pointer" }}
           />
-          <PhotoRoundedIcon
-            sx={{ fontSize: 40, color: "##212121", cursor: "pointer" }}
+          <input
+            type="file"
+            multiple
+            accept="image/*"
+            style={{ display: "none" }}
+            id="icon-button-file"
+            onChange={handleImageUpload}
           />
+          <label htmlFor="icon-button-file">
+            <PhotoRoundedIcon
+              sx={{ fontSize: 40, color: "##212121", cursor: "pointer" }}
+            />
+          </label>
           <SmartDisplayRoundedIcon
             sx={{ fontSize: 40, color: "##212121", cursor: "pointer" }}
           />
@@ -74,6 +102,26 @@ const AddDiaryPage = () => {
           </Button>
         </S.SubmitBtnWrapper>
       </S.TagnIconsContainer>
+      {images.length > 0 && (
+        <S.PreviewnBtnContainer>
+          업로드된 사진 미리보기
+          <S.PreviewContainer>
+            <S.PrevButton onClick={prevPage} disabled={currentPage === 0}>
+              <ArrowBackIosNewRoundedIcon sx={{ fontSize: 40 }} />
+            </S.PrevButton>
+            <S.ImagePreview src={images[currentPage]} alt="preview" />
+            <S.DeleteButton onClick={handleImageDelete}>
+              <DeleteIcon />
+            </S.DeleteButton>
+            <S.NextButton
+              onClick={nextPage}
+              disabled={currentPage === images.length - 1}
+            >
+              <ArrowForwardIosRoundedIcon sx={{ fontSize: 40 }} />
+            </S.NextButton>
+          </S.PreviewContainer>
+        </S.PreviewnBtnContainer>
+      )}
     </S.InputContainer>
   );
 };
--- src/pages/AddDiaryPage.styled.js
+++ src/pages/AddDiaryPage.styled.js
@@ -39,3 +39,57 @@ export const IconsContainer = styled.div`
 export const SubmitBtnWrapper = styled.div`
   justify-content: flex-end;
 `;
+
+export const PreviewnBtnContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+  position: relative;
+`;
+
+export const PreviewContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const PrevButton = styled.button`
+  width: fit-content;
+  position: absolute;
+  left: 0vw;
+  padding: 10px 20px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+export const NextButton = styled(PrevButton)`
+  left: 38vw;
+`; // PrevButton과 동일한 스타일을 사용
+
+export const DeleteButton = styled.button`
+  position: absolute;
+  left: 40vw;
+  bottom: 0.5vh;
+  padding: 10px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  background-color: transparent;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const ImagePreview = styled.img`
+  width: 43vw;
+  height: 35vh;
+  object-fit: contain;
+  background-color: rgba(0, 0, 0, 0.1);
+`;
