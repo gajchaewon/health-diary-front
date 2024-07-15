c073973860bdfbd1a68e2e80c67c5e04d555058c
twonezero <dnjsdudtm98@naver.com>
2024-07-15 15:46:27 +0900

    #52 - fix : 리프레시 기능 동작하도록 수정
    * 서버에서 반환하는 에러 코드에 대해 토큰 리프레시 기능이 정상적으로 일어나도록 수정함.

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logIn, logOut } from "../features/auth/authSlice";
-import { HttpStatusCode } from "axios";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8080",
@@ -17,13 +16,13 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithReAuth = async (args, api) => {
   let result = await baseQuery(args, api);
   if (result?.error?.data !== null) {
-    if (result?.error?.data.error === "EXPIRED_TOKEN") {
+    if (result?.error?.data.errorCode === "EXPIRED_TOKEN") {
       const refreshResult = await baseQuery("/auth/refresh-token", api);
       const userResponse = api.getState().auth.userInfo;
-
+      console.log(refreshResult);
       const tokenResponse = {
         accessToken: refreshResult.data?.accessToken,
-        refreshToken: null,
+        refreshToken: refreshResult.data?.refreshToken,
       };
 
       api.dispatch(logIn({ tokenResponse, userResponse }));
