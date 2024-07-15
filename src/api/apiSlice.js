720ffcbd4feab55961cb80ca16bbc56e90fe7425
twonezero <dnjsdudtm98@naver.com>
2024-07-15 21:13:17 +0900

    #52 - apiSlice : 리프레시 시 서버에서 반환하는 errorCode 조건 추가
    * 로그아웃 후 홈페이지로 이동하도록 수정
    * alert() 추가

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -13,30 +13,43 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const errorConditions = {
+  EXPIRED_TOKEN: "EXPIRED_TOKEN",
+  WRONG_TYPE_TOKEN: "WRONG_TYPE_TOKEN",
+  UNSUPPORTED_TOKEN: "UNSUPPORTED_TOKEN",
+  UNKNOWN_ERROR: "UNKNOWN_ERROR",
+  REFRESH_TOKEN_NULL: "AUTH_001",
+  TOKEN_NOT_FOUND: "AUTH_002",
+  TOKEN_EXTRACT_FAILED: "AUTH_003",
+  TOKEN_NOT_VALID: "AUTH_004",
+  REFRESH_LOGOUT: "AUTH_005",
+  REFRESH_FAILED: "AUTH_006",
+};
+
 const baseQueryWithReAuth = async (args, api) => {
   let result = await baseQuery(args, api);
-  if (result?.error?.data !== null) {
-    if (result?.error?.data.errorCode === "EXPIRED_TOKEN") {
+  if (result.error?.data.errorCode !== null) {
+    const errorCode = result.error?.data.errorCode;
+    // 기본적으로 만료되었을 때 리프레시
+    if (errorCode === errorConditions.EXPIRED_TOKEN) {
       const refreshResult = await baseQuery("/auth/refresh-token", api);
       const userResponse = api.getState().auth.userInfo;
-      console.log(refreshResult);
-      const tokenResponse = {
-        accessToken: refreshResult.data?.accessToken,
-        refreshToken: refreshResult.data?.refreshToken,
-      };
-
-      api.dispatch(logIn({ tokenResponse, userResponse }));
-      result = await baseQuery(args, api);
-    } else if (
-      [
-        "WRONG_TYPE_TOKEN",
-        "UNSUPPORTED_TOKEN",
-        "UNKNOWN_ERROR",
-        "ACCESS_DENIED",
-      ].includes(result?.error?.data.error)
-    ) {
-      alert("오류로 인해 로그아웃됩니다.");
+      if (refreshResult?.data) {
+        const tokenResponse = {
+          accessToken: refreshResult.data.accessToken,
+          refreshToken: refreshResult.data.refreshToken,
+        };
+        api.dispatch(logIn({ tokenResponse, userResponse }));
+        result = await baseQuery(args, api);
+      } else {
+        alert("로그인 정보가 만료되었습니다. 다시 로그인 해주세요.");
+        api.dispatch(logOut());
+        window.location = "/";
+      }
+    } else if (Object.values(errorConditions).includes(errorCode)) {
+      alert("다시 로그인 해주세요.");
       api.dispatch(logOut());
+      window.location = "/";
     }
   }
   return result;
