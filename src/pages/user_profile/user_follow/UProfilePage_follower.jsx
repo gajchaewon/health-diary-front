8e35a1cb1133e83a0b9719e51b81687d85eba49e
CHAEEUNPAKR <pce2416@gmail.com>
2024-08-28 01:39:31 +0900

    #59 - follower, following 컴포넌트 수정

--- src/pages/user_profile/user_follow/UProfilePage_follower.jsx
+++ src/pages/user_profile/user_follow/UProfilePage_follower.jsx
@@ -3,6 +3,7 @@ import UserCard from "../../../components/follow/FollowUserCard";
 import { useGetFollowersQuery } from "../../../features/users/userApiSlice";
 import { useParams } from "react-router-dom";
 import * as GlobalSytle from "../UserProfile.style";
+import FollowUserCard from "../../../components/follow/FollowUserCard";
 
 const UProfilePageFollower = () => {
   const userId = useParams().userId;
@@ -14,7 +15,7 @@ const UProfilePageFollower = () => {
       {userFollowerList &&
         userFollowerList?.map((user) => (
           <div key={user?.id}>
-            <UserCard nickname={user?.nickname} userId={user?.id} />
+            <FollowUserCard nickname={user?.nickname} userId={user?.id} />
           </div>
         ))}
     </GlobalSytle.Container>
